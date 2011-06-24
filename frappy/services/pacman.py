9b5af00b042abe86ff3c1bec394dadc4a5ce4175
Luke Lee <durdenmisc@gmail.com>
2011-06-24 16:19:31 -0500

    Don't use debug for pacman service

--- frappy/services/pacman.py
+++ frappy/services/pacman.py
@@ -10,7 +10,7 @@ class Pacman(APICall):
     """
     """
     def __init__(self, req_format="json", domain="127.0.0.1:8000",
-                 secure=False, auth=None, api_version=_DEFAULT):
+                 secure=False, auth=None, api_version=_DEFAULT, debug=False):
 
         APICall.__init__(self, auth=auth, req_format=req_format, domain=domain,
-                         secure=secure)
+                         secure=secure, debug=debug)
