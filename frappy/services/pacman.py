e01c2049f9b8c9b576c889433db14d1b6ae7f796
Luke Lee <durdenmisc@gmail.com>
2011-06-27 15:33:01 -0500

    Add debug argument to main Pacman __init__

--- frappy/services/pacman.py
+++ frappy/services/pacman.py
@@ -9,7 +9,8 @@ class Pacman(APICall):
     """
     """
     def __init__(self, req_format="json", domain="127.0.0.1:8000",
-                 secure=False, auth=None, api_version=DefaultVersion):
+                 secure=False, auth=None, api_version=DefaultVersion,
+                 debug=False):
 
         APICall.__init__(self, auth=auth, req_format=req_format, domain=domain,
                          secure=secure, debug=debug)
