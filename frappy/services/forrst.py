db47fbf2dbd4c6ff061995ad81600e666bf3d5a6
Luke Lee <durdenmisc@gmail.com>
2011-06-27 23:01:18 -0500

    Basic support for Forrst API added

---
+++ frappy/services/forrst.py
@@ -0,0 +1,29 @@
+"""
+Class to perform operations on the Forrst API
+"""
+
+from frappy.core.api import APICall, DefaultVersion
+
+
+class Forrst(APICall):
+    """
+    Tiny wrapper around Forrst API
+    """
+
+    def __init__(self, req_format="json", domain="forrst.com/api",
+                 secure=False, auth=None, api_version=DefaultVersion):
+
+        if api_version is DefaultVersion:
+            api_version = 'v2'
+
+        uriparts = ()
+        if api_version:
+            uriparts += (str(api_version),)
+
+        APICall.__init__(self, auth=auth, req_format=req_format, domain=domain,
+                         secure=secure, uriparts=uriparts)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
