2ea813570cce2342b3931aa569135759ebe083ea
Luke Lee <durdenmisc@gmail.com>
2011-07-05 22:14:12 -0500

    No verison argument needed for fake pacman service

--- frappy/services/pacman.py
+++ frappy/services/pacman.py
@@ -2,15 +2,14 @@
 Wrapper for fictional test service
 """
 
-from frappy.core.api import APICall, DefaultVersion
+from frappy.core.api import APICall
 
 
 class Pacman(APICall):
-    """
-    """
+    """Wrapper for Pacman service API"""
+
     def __init__(self, req_format="json", domain="127.0.0.1:8000",
-                 secure=False, auth=None, api_version=DefaultVersion,
-                 debug=False):
+                 secure=False, auth=None, debug=False):
 
         APICall.__init__(self, auth=auth, req_format=req_format, domain=domain,
                          secure=secure, debug=debug)
