3b378fdd58028130b7a71ac5f91e9a978fba2a6d
Luke Lee <durdenmisc@gmail.com>
2011-07-06 23:04:17 -0500

    Add basic tests for forrst api

--- frappy/services/forrst.py
+++ frappy/services/forrst.py
@@ -8,6 +8,30 @@ from frappy.core.api import APICall, DefaultVersion
 class Forrst(APICall):
     """
     Tiny wrapper around Forrst API
+
+    >>> from frappy.services.forrst import Forrst
+    >>> f = Forrst()
+    >>> x = f.stats()
+    >>> x.response['stat']
+    u'ok'
+    >>> x = f.users.info(username='durden')
+    >>> x.response['stat']
+    u'ok'
+    >>> x.response['resp']['username']
+    u'durden'
+    >>> x = f.posts.show(id=45114)
+    >>> x.response['stat']
+    u'ok'
+    >>> 'content' in x.response['resp']
+    True
+    >>> x = f.posts.all()
+    >>> x.response['stat']
+    u'ok'
+    >>> x = f.posts.list(post_type='code')
+    >>> x.response['stat']
+    u'ok'
+    >>> 'posts' in x.response['resp']
+    True
     """
 
     def __init__(self, req_format="json", domain="forrst.com/api",
