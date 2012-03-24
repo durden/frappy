078246caa03a05142114321bb8cfee128584167f
Luke Lee <durdenmisc@gmail.com>
2012-03-23 23:05:30 -0500

    Make username argument part of init since api_key and username are unique pair
    - The codrspace api currently only supports getting your own posts.  So, the
      api_key must match exactly with the username it was given.  Thus, we don't
      need it as an argument to the api calls, just the api object.

--- frappy/services/codrspace.py
+++ frappy/services/codrspace.py
@@ -16,8 +16,8 @@ class Codrspace(APICall):
     ----------------
 
     >>> from frappy.services.codrspace import Codrspace
-    >>> c = Codrspace('c139439a4d682d2db84a8d603eddb5c38c56d8e5')
-    >>> posts = c.post(username='durden')
+    >>> c = Codrspace('durden', 'c139439a4d682d2db84a8d603eddb5c38c56d8e5')
+    >>> posts = c.post()
     >>> 'title' in posts.response['objects'][0]
     True
     >>> # Loop over posts
@@ -27,12 +27,13 @@ class Codrspace(APICall):
 
     """
 
-    def __init__(self, api_key, domain="codrspace.com/api/"):
+    def __init__(self, username, api_key, domain="codrspace.com/api/"):
 
         APICall.__init__(self, auth=None, req_format='json', domain=domain,
                          secure=False)
 
         self._api_key = api_key
+        self._username = username
 
     def _prepare_request_params(self, **kwargs):
         """Append mandatory arguments for format"""
@@ -40,6 +41,7 @@ class Codrspace(APICall):
         # get requests are just added to the uri as normal, but post pass JSON
         kwargs['api_key'] = self._api_key
         kwargs['format'] = 'json'
+        kwargs['username'] = self._username
 
         return kwargs
 
